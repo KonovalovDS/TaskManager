4215e27939e9e7392ff6ffe85505ba8d8c0dc83b
Dimasik322 <dimakonovalov88@gmail.com>
2025-03-21 14:21:30 +0400

    0.0.1

--- TaskManager/App.xaml.cs
+++ TaskManager/App.xaml.cs
@@ -7,6 +7,50 @@ namespace TaskManager {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        public static int taskCounter = 0;
+        public static int complitedTaskCounter = 0;
+
+        public static Dictionary<int, string> data = new Dictionary<int, string>();
+        private static MainWindow window = new MainWindow();
+
+        public class DeadlineItem {
+            public int Id { get; set; }
+            public required string DeadlineText { get; set; }
+        }
+
+        public App() {
+            InitializeComponent();
+        }
+
+        public static void AddTask(int id, string text) {
+            App.taskCounter++;
+            App.data.Add(id, text);
+            window.UpdateProgressBar();
+        }
+
+        public static void DeleteTask(int id) {
+            if (App.taskCounter > 0) { App.taskCounter--; }
+            App.data.Remove(id);
+            window.UpdateProgressBar();
+
+        }
+
+        public static void CompleteTask(int id) {
+            DeleteTask(id);
+            App.complitedTaskCounter++;
+            window.UpdateProgressBar();
+        }
+
+        private void LoadData() {
+            App.data.Clear();
+            //App.data = getData();
+            window.UpdateProgressBar();
+        }
+        private void UpdateData() {
+            //App.data
+        }
+
     }
 
 }
--- TaskManager/MainWindow.xaml.cs
+++ TaskManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,14 +9,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static TaskManager.App;
 
 namespace TaskManager {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        public MainWindow() {
+        public MainWindow() { 
             InitializeComponent();
+        }
+
+        public ObservableCollection<DeadlineItem> Deadlines { get; set; } = new ObservableCollection<DeadlineItem>();
+
+        public void UpdateProgressBar() {
+            if (App.complitedTaskCounter > App.taskCounter) {
+                Console.WriteLine("Error: complitedTaskCounter > taskCounter");
+            }
+            else {
+                WeeklyProgressBar.Value = App.complitedTaskCounter / App.taskCounter;
+            }
         }
 
         private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
@@ -23,6 +36,27 @@ namespace TaskManager {
             if (scrollViewer != null) {
                 scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta / 3);
                 e.Handled = true;
+            }
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e) {
+            DateTime date = DateTime.Now;
+            //string msg = ;
+            string text = $"{date.Day}.{date.Month} - ";
+            int id = 0;
+            while (App.data.ContainsKey(id)) {
+                id++;
+            }
+            App.AddTask(id, text);
+            Deadlines.Add(new DeadlineItem { Id = id, DeadlineText = text });
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e) {
+            int id = 0;
+            var itemToRemove = Deadlines.FirstOrDefault(item => item.Id == id);
+            if (itemToRemove != null) {
+                App.DeleteTask(id);
+                Deadlines.Remove(itemToRemove);
             }
         }
 
